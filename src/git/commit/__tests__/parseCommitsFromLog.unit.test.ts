3645f0324633d00133dfb6b89337a95c3df7f974
William Thorsen <william@thorsen.dev>
2020-02-10 12:50:35 -0700

    fix: Change level is incorrectly computed at stable HEAD
    A push to a prerelease branch at the same level as a stable release no longer results in a version bump

---
+++ src/git/commit/__tests__/parseCommitsFromLog.unit.test.ts
@@ -0,0 +1,22 @@
+import { GitCommit } from '../../types';
+import { parseCommitsFromLog } from '../parseCommitsFromLog';
+
+describe('parseCommitsFromLog', () => {
+  it('should return an empty array if the log is empty', () => {
+    const commitLog = '';
+
+    const parsedCommits = parseCommitsFromLog(commitLog);
+
+    const expectedCommits: GitCommit[] = [];
+    expect(parsedCommits).toEqual(expectedCommits);
+  });
+
+  it('should return an empty array if the log consists only of a newline', () => {
+    const commitLog = '\n';
+
+    const parsedCommits = parseCommitsFromLog(commitLog);
+
+    const expectedCommits: GitCommit[] = [];
+    expect(parsedCommits).toEqual(expectedCommits);
+  });
+});
--- src/git/commit/parseCommitsFromLog.ts
+++ src/git/commit/parseCommitsFromLog.ts
@@ -2,7 +2,12 @@ import { GitCommit } from '../types';
 import { parseCommitFromLogEntry } from './parseCommitFromLogEntry';
 
 export function parseCommitsFromLog(commitLog: string): GitCommit[] {
+  if (!commitLog) {
+    return [];
+  }
+
   return commitLog
     .split('\n')
+    .filter((logEntry) => logEntry) // filter out empty entries
     .map((logEntry) => parseCommitFromLogEntry(logEntry));
 }
